f74333c557764a8f88aeaeee9b8af01135834ae8
SanthoshVallamkonda <86008642+SanthoshVallamkonda@users.noreply.github.com>
2021-07-10 13:37:42 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -4,3 +4,9 @@ def add(x, y) {
 def sub(x,y) {
    echo "Substraction of ${x} and ${y} is ${x - y}"
 }
+def mul(x, y) {
+  echo "Multiplication of ${x} and ${y} is ${x * y}"
+}
+def div(x, y) {
+  echo "Division of ${x} and ${y} is ${x / y}"
+}
