0ef27190406aa39c3950e4f7d98aa71426bd53f6
SanthoshVallamkonda <86008642+SanthoshVallamkonda@users.noreply.github.com>
2021-07-10 14:12:05 +0530

    Update uploadToNexus.groovy

--- vars/uploadToNexus.groovy
+++ vars/uploadToNexus.groovy
@@ -2,10 +2,10 @@ def call(nexusRepo){
   def pom = readMavenPom file: 'pom.xml'
   def artifacdId = pom.artifactId
   def version = pom.version
-  def package = pom.packaging
+  def packaged = pom.packaging
   def groupdId = pom.groupId
   def repo = nexusRepo//pom.version.endsWith('SNAPSHOT') ? "my-app-snapshot" : "my-app-release"
-  nexusArtifactUploader artifacts: [[artifactId: artifacdId, classifier: '', file: "target/${artifacdId}-${version}.${package}", type: package]], 
+  nexusArtifactUploader artifacts: [[artifactId: artifacdId, classifier: '', file: "target/${artifacdId}-${version}.${packaged}", type: packaged]], 
   credentialsId: 'nexus3', 
   groupId: groupdId, 
   nexusUrl: '172.31.44.155:8081', 
